55e0398bec69f5c9dec8fada55544101b2700c8e
Vishal Gupta <vkg051198@gmail.com>
2023-08-30 21:19:56 +0530

    JS-9 DOM

---
+++ 4) JavaScript/27. JavaScript-9/Topics/topic.js
@@ -0,0 +1,6 @@
+let smallImages = document.getElementsByClassName("oldImg");
+
+for(let i=0; i<smallImages.length; i++){
+    smallImages[i].src = "https://www.pngmart.com/files/7/Ceramic-Lamp-PNG-File.png";
+    console.log(`value of image no. ${i} is changed.`)
+}
