380b653629e3e2c8ba215f44bea5a6a92fe6f9d4
Vishal Gupta <vkg051198@gmail.com>
2023-09-01 12:31:48 +0530

    JS-9 DOM

--- 4) JavaScript/27. JavaScript-9/Topics/topic.js
+++ 4) JavaScript/27. JavaScript-9/Topics/topic.js
@@ -1,6 +1,11 @@
-let smallImages = document.getElementsByClassName("oldImg");
+// getElementByClassName
+// let smallImages = document.getElementsByClassName("oldImg");
 
-for(let i=0; i<smallImages.length; i++){
-    smallImages[i].src = "https://www.pngmart.com/files/7/Ceramic-Lamp-PNG-File.png";
-    console.log(`value of image no. ${i} is changed.`)
-}
+// for(let i=0; i<smallImages.length; i++){
+//     smallImages[i].src = "https://www.pngmart.com/files/7/Ceramic-Lamp-PNG-File.png";
+//     console.log(`value of image no. ${i} is changed.`)
+// }
+
+// -----------------------------------------------------------
+
+// console.dir(document.querySelectorAll("div b"));
