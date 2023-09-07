755ab67916c40ed6d6447f530486d3c9b4c721bc
Vishal Gupta <vkg051198@gmail.com>
2023-09-07 19:11:15 +0530

    JS-9 DOM-assignment Questions

--- 4) JavaScript/27. JavaScript-9/JS-9 (Ques)/ques.js
+++ 4) JavaScript/27. JavaScript-9/JS-9 (Ques)/ques.js
@@ -1,3 +1,34 @@
+// Ans-1
 let button = document.createElement("button");
 let input = document.createElement("input");
 button.innerText = "Click me";
+
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+// -----------------------------------------------
+
+// Ans-2
+button.setAttribute("id", "btn");
+input.setAttribute("placeholder", "username");
+
+// -----------------------------------------------
+
+// Ans-3
+let btn = document.querySelector("#btn");
+btn.classList.add("btnStyle");
+
+// -----------------------------------------------
+
+// Ans-4
+let h1 = document.createElement("h1");
+h1.innerHTML = "<u>DOM Practice</u>";
+document.querySelector("body").append(h1);
+
+// -----------------------------------------------
+
+// Ans-5
+let p = document.createElement("p");
+p.innerHTML = "Apna College <b>Delta</b> Practice";
+document.querySelector("body").append(p);
+
