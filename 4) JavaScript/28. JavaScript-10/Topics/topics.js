973a10d3de815acf526a58ccae03287949020998
Vishal Gupta <vkg051198@gmail.com>
2023-09-08 09:15:46 +0530

    JS-10 Event Listener

--- 4) JavaScript/28. JavaScript-10/Topics/topics.js
+++ 4) JavaScript/28. JavaScript-10/Topics/topics.js
@@ -1,20 +1,33 @@
-// let btn = document.querySelector("button");
-// // console.dir(btn);
+// // let btn = document.querySelector("button");
+// // // console.dir(btn);
 
-let btns = document.querySelectorAll("button");
-for(btn of btns){
-    btn.onclick = sayHello;
-    btn.onmouseenter = function() {
-        console.log("your entered a button");
-    };
-}
-
-// btn.onclick = function() {
-//     alert("button was clicked");
+// let btns = document.querySelectorAll("button");
+// for(btn of btns){
+//     btn.onclick = sayHello;
+//     btn.onmouseenter = function() {
+//         console.log("your entered a button");
+//     };
 // }
+
+// // btn.onclick = function() {
+// //     alert("button was clicked");
+// // }
 
 function sayHello() {
     alert("Hello!");
-}
+};
+function sayName() {
+    alert("Apna College");
+};
 
-// btn.onclick = sayHello;
+// // btn.onclick = sayHello;
+
+// Event Listener
+let btns = document.querySelectorAll('button');
+for(btn of btns) {
+    // btn.addEventListener("click",  sayHello);
+    // btn.addEventListener("click",  sayName);
+    btn.addEventListener("dblclick",  function(){
+        console.log("double clicked the action")
+    });
+}
