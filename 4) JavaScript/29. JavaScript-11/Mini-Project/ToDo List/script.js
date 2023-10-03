26ed1ea01b4f5e11e8539a4857ba9fa0ecb10dcb
Vishal Gupta <vkg051198@gmail.com>
2023-10-03 11:22:09 +0530

    DoTo App Completed

--- 4) JavaScript/29. JavaScript-11/Mini-Project/ToDo List/script.js
+++ 4) JavaScript/29. JavaScript-11/Mini-Project/ToDo List/script.js
@@ -5,8 +5,29 @@ let inp = document.querySelector("input");
 btn.addEventListener("click", function() {
     let item = document.createElement("li");
     item.innerText = inp.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
     ul.appendChild(item);
-    console.log(inp.value);
     inp.value = "";
 })
 
+// Event Delegation
+ul.addEventListener("click", function(event) {
+    if(event.target.nodeName == "BUTTON") {
+        let listItem = event.target.parentElement;
+        listItem.remove();
+    }
+})
+
+// let delBtns = document.querySelectorAll(".delete");
+// for(delBtn of delBtns) {
+//     delBtn.addEventListener("click", function() {
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     })
+// }
